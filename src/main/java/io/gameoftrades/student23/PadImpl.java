4a694dbd221a6f9a2ef221fc67073af5b0baae7f
freedow10 <danieltang68372@gmail.com>
2016-09-16 00:14:25 +0200

    Begin files voor opdracht2

---
+++ src/main/java/io/gameoftrades/student23/PadImpl.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.gameoftrades.student23;
+
+import io.gameoftrades.model.kaart.Coordinaat;
+import io.gameoftrades.model.kaart.Pad;
+import io.gameoftrades.model.kaart.Richting;
+
+/**
+ *
+ * @author Daniel
+ */
+public class PadImpl implements Pad{
+
+    @Override
+    public int getTotaleTijd() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Richting[] getBewegingen() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Pad omgekeerd() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Coordinaat volg(Coordinaat start) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/main/java/io/gameoftrades/student23/SnelstePadAlgoritmeImpl.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.gameoftrades.student23;
+
+import io.gameoftrades.model.algoritme.SnelstePadAlgoritme;
+import io.gameoftrades.model.kaart.Coordinaat;
+import io.gameoftrades.model.kaart.Kaart;
+import io.gameoftrades.model.kaart.Pad;
+
+/**
+ *
+ * @author Daniel
+ */
+public class SnelstePadAlgoritmeImpl implements SnelstePadAlgoritme{
+
+    @Override
+    public Pad bereken(Kaart kaart, Coordinaat start, Coordinaat eind) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
