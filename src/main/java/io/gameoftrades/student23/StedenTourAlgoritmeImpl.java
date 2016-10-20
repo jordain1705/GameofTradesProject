d01b1cf7027fb286dfc1b6a4a7de43fba3bda17f
Jordain Gijsbertha <JordainGijsbertha@jordains-mbp.wireless.hhs.nl>
2016-10-20 15:05:05 +0200

    Refacoring

--- src/main/java/io/gameoftrades/student23/StedenTourAlgoritmeImpl.java
+++ src/main/java/io/gameoftrades/student23/StedenTourAlgoritmeImpl.java
@@ -24,96 +24,92 @@ import java.util.Map.Entry;
  */
 public class StedenTourAlgoritmeImpl implements StedenTourAlgoritme, Debuggable {
 
-    
     private Debugger debug = new AsciiArtDebugger();
-    private  List<Stad> StedenTourPad ;
-    private  SnelstePadAlgoritmeImpl SnelstePimpl;
-    private  Map<Stad, Double> map ;
-    
-    Map<List<Stad>, Double> beginstadmap =new HashMap<>();
-     
-     
-   private  List<Stad> kortsteafstandenstad ;
-   private  PadImpl pad;
+    private List<Stad> StedenTourPad;
+    private SnelstePadAlgoritmeImpl SnelstePimpl;
+    private Map<Stad, Double> map;
+
+    Map<List<Stad>, Double> beginstadmap = new HashMap<>();
+
+    private List<Stad> kortsteafstandenstad;
+    private PadImpl pad;
 
     public StedenTourAlgoritmeImpl() {
         kortsteafstandenstad = new ArrayList<>();
-        map= new HashMap<>();
+        map = new HashMap<>();
         SnelstePimpl = new SnelstePadAlgoritmeImpl();
-     
+
     }
 
     @Override
     public List<Stad> bereken(Kaart kaart, List<Stad> steden) {
-         List<Stad> tour = findNearest(kaart, steden);
-        
-         debug.debugSteden(kaart, tour);
-        
-        return tour ;
+        List<Stad> tour = findNearest(kaart, steden);
+
+        debug.debugSteden(kaart, tour);
+
+        return tour;
     }
 
     public List<Stad> findNearest(Kaart kaart, List<Stad> list) {
 
         for (int j = 0; j < list.size(); j++) {
             kortsteafstandenstad.clear();
-             List<Stad> lijstvanSteden = new ArrayList();
-             lijstvanSteden.addAll(list);
-            ListIterator<Stad> iterator = lijstvanSteden.listIterator();
+            List<Stad> lijstvanSteden = new ArrayList();
+            lijstvanSteden.addAll(list);
             Stad startstad = lijstvanSteden.get(j);
             kortsteafstandenstad.add(startstad);
-        double kosten = 0;
-         lijstvanSteden.remove(j);
-        Boolean isdone = false;
-        while (!isdone) {
+            double kosten = 0;
+            lijstvanSteden.remove(j);
+            Boolean isdone = false;
+            while (!isdone) {
 
-            for (int i = 0; i < lijstvanSteden.size(); i++) {
-                Stad get = lijstvanSteden.get(i);
-                pad = SnelstePimpl.aStarAlgoritme(kaart, startstad.getCoordinaat(), get.getCoordinaat());
-                double gvalue = pad.getPathGValue();
-                map.put(get, gvalue);
-             
-            }
-       
-            startstad = MinGvalueCity().getKey();
-            kosten = kosten + MinGvalueCity().getValue();
-            kortsteafstandenstad.add(startstad);
-            lijstvanSteden.remove(startstad);
-            map.clear();
+                for (int i = 0; i < lijstvanSteden.size(); i++) {
+                    Stad get = lijstvanSteden.get(i);
+                    pad = SnelstePimpl.aStarAlgoritme(kaart, startstad.getCoordinaat(), get.getCoordinaat());
+                    double gvalue = pad.getPathGValue();
+                    map.put(get, gvalue);
 
-            //Print kosten
-            if (lijstvanSteden.isEmpty()) {
-                List<Stad> bestekorste = kortsteafstandenstad;
-                beginstadmap.put(bestekorste,kosten);
-                isdone = true;
+                }
+
+                startstad = MinGvalueCity().getKey();
+                kosten = kosten + MinGvalueCity().getValue();
+                kortsteafstandenstad.add(startstad);
+                lijstvanSteden.remove(startstad);
+                map.clear();
+
+                //Print kosten
+                if (lijstvanSteden.isEmpty()) {
+                    List<Stad> bestekorste = kortsteafstandenstad;
+                    beginstadmap.put(bestekorste, kosten);
+                    isdone = true;
+                }
             }
         }
-    }
-        System.out.println("Dit is de beste route de beste kosten en  de beste beginstad :"+bestebeginstad().getValue());
-        StedenTourPad=bestebeginstad().getKey();
+        System.out.println("Dit is de beste route de beste kosten en  de beste beginstad :" + bestebeginstad().getValue());
+        StedenTourPad = bestebeginstad().getKey();
         return StedenTourPad;
     }
 
     public Entry<Stad, Double> MinGvalueCity() {
-         Map.Entry<Stad, Double> minEntry = null;
+        Map.Entry<Stad, Double> minEntry = null;
         for (Map.Entry<Stad, Double> entry : map.entrySet()) {
-   if (minEntry == null || entry.getValue() < minEntry.getValue()) {
-      minEntry = entry;
-   }
-   
- }
-        return minEntry;
-    }
-   
-     public Entry<List<Stad>, Double> bestebeginstad() {
-         Map.Entry<List<Stad>, Double> minEntry = null;
-        for (Entry<List<Stad>, Double> entry : beginstadmap.entrySet()) {
-             if (minEntry == null || entry.getValue() < minEntry.getValue()) {
-      minEntry = entry;
-   }
+            if (minEntry == null || entry.getValue() < minEntry.getValue()) {
+                minEntry = entry;
+            }
+
         }
         return minEntry;
     }
 
+    public Entry<List<Stad>, Double> bestebeginstad() {
+        Map.Entry<List<Stad>, Double> minEntry = null;
+        for (Entry<List<Stad>, Double> entry : beginstadmap.entrySet()) {
+            if (minEntry == null || entry.getValue() < minEntry.getValue()) {
+                minEntry = entry;
+            }
+        }
+        return minEntry;
+    }
 
     @Override
     public void setDebugger(Debugger debugger) {
